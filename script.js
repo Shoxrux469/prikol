d9a7d6c22b546358ac7fab8083ab7a40e9931e87
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-09-30 18:08:16 +0500

    bibi

--- script.js
+++ script.js
@@ -18,14 +18,19 @@ let h_right = document.createElement('div')
 h_right.classList.add('h_right')
 let h_btns = document.createElement('div')
 h_btns.classList.add('h_btns')
-let h_text = document.createElement('div')
 h_text.classList.add('h_text')
+let h_text = document.createElement('div')
 let h_btns_button1 = document.createElement('button')
+h_btns_button1.innerHTML = '0% Рассрочка'
 let h_btns_button2 = document.createElement('button')
+h_btns_button2.innerHTML = 'Скидки'
 h_btns_button2.classList.add('discounts')
 let h_btns_button3  = document.createElement('button')
+h_btns_button3 .innerHTML ='Розыгрыши'
 let h_text_span1 = document.createElement('span')
+h_text_span1.innerHTML = 'Карта сайта'
 let h_text_span2 = document.createElement('span')
+h_text_span2.innerHTML = 'Выберите регион'
 let h_text_span1_br = document.createElement('br')
 let h_text_span2_br = document.createElement('br')
 let h_mid_svg = document.createElement('svg')
@@ -36,10 +41,15 @@ let h_mid_svg_path = document.createElement('path')
 h_mid_svg_path.d = 'M12.7105 15.54L18.3605 9.87998C18.4542 9.78702 18.5286 9.67642 18.5793 9.55456C18.6301 9.4327 18.6562 9.30199 18.6562 9.16998C18.6562 9.03797 18.6301 8.90726 18.5793 8.78541C18.5286 8.66355 18.4542 8.55294 18.3605 8.45998C18.1731 8.27373 17.9196 8.16919 17.6555 8.16919C17.3913 8.16919 17.1378 8.27373 16.9505 8.45998L11.9505 13.41L7.00045 8.45998C6.81309 8.27373 6.55964 8.16919 6.29545 8.16919C6.03127 8.16919 5.77781 8.27373 5.59045 8.45998C5.49596 8.5526 5.42079 8.66304 5.3693 8.78492C5.3178 8.90679 5.291 9.03767 5.29045 9.16998C5.291 9.30229 5.3178 9.43317 5.3693 9.55505C5.42079 9.67692 5.49596 9.78737 5.59045 9.87998L11.2405 15.54C11.3341 15.6415 11.4477 15.7225 11.5742 15.7779C11.7007 15.8333 11.8373 15.8619 11.9755 15.8619C12.1136 15.8619 12.2502 15.8333 12.3767 15.7779C12.5032 15.7225 12.6168 15.6415 12.7105 15.54Z'
 h_mid_svg_path.fill = 'white'
 let h_mid_p = document.createElement('p')
+h_mid_p.innerHTML = '+998 (71) 202 202 1'
 let h_mid_button = document.createElement('button')
+h_mid_button.innerHTML = 'Продавайте на olcha'
 let h_right_span1 = document.createElement('span')
+h_right_span1.innerHTML = 'Узб'
 let h_right_span2 = document.createElement('span')
+h_right_span2.innerHTML = 'Oz'
 let h_right_span3 = document.createElement('span')
+h_right_span3.innerHTML = 'Rus'
 h_right_span3.classList.add('white')
 let head_down_a = document.createElement('a')
 head_down_a.href = '#'
@@ -58,6 +68,7 @@ let head_down_button_div_span1 = document.createElement('span')
 let head_down_button_div_span2 = document.createElement('span')
 let head_down_button_div_span3 = document.createElement('span')
 let head_down_button_p = document.createElement('p')
+head_down_button_p.innerHTML = 'Каталог'
 let head_down_div1 = document.createElement('div')
 head_down_div1.classList.add('searcher')
 let head_down_div1_inp = document.createElement('inp')
@@ -89,6 +100,7 @@ for(let i = 0;i <= 3; i++) {
     head_down_div2_svg.append(rect)
 }
 let head_down_div2_p = document.createElement('p')
+head_down_div2_p.innerHTML = 'Сравнение'
 let head_down_div3 = document.createElement('div')
 head_down_div3.classList.add('favourites si')
 let head_down_div3_svg = document.createElement('svg')
@@ -99,6 +111,7 @@ head_down_div3_svg.classList.add('icon_favorites')
 let head_down_div3_svg_path = document.createElement('path')
 head_down_div3_svg_path.d = 'M21.2913 4.61183C20.7805 4.10083 20.1741 3.69547 19.5066 3.41891C18.8392 3.14235 18.1238 3 17.4013 3C16.6788 3 15.9634 3.14235 15.2959 3.41891C14.6285 3.69547 14.022 4.10083 13.5113 4.61183L12.4513 5.67183L11.3913 4.61183C10.3596 3.58013 8.96032 3.00053 7.50129 3.00053C6.04226 3.00053 4.64298 3.58013 3.61129 4.61183C2.5796 5.64352 2 7.04279 2 8.50183C2 9.96086 2.5796 11.3601 3.61129 12.3918L4.67129 13.4518L12.4513 21.2318L20.2313 13.4518L21.2913 12.3918C21.8023 11.8811 22.2076 11.2746 22.4842 10.6072C22.7608 9.93972 22.9031 9.22431 22.9031 8.50183C22.9031 7.77934 22.7608 7.06393 22.4842 6.39647C22.2076 5.72901 21.8023 5.12258 21.2913 4.61183V4.61183Z'
 let head_down_div3_p = document.createElement('p')
+head_down_div3_p.innerHTML = 'Избранные'
 let head_down_div4 = document.createElement('div') 
 head_down_div4.classList.add('bag si')
 let head_down_div4_span = document.createElement('span')
@@ -113,6 +126,7 @@ head_down_div4_svg_path2.d = 'M15 18.75C15.8629 18.75 16.5625 18.0504 16.5625 17
 let head_down_div4_svg_path3 = document.createElement('path')
 head_down_div4_svg_path3.d = 'M3.125 5H16.7512C16.8427 5 16.9332 5.02011 17.0161 5.0589C17.099 5.0977 17.1724 5.15423 17.231 5.22451C17.2897 5.29479 17.3322 5.37709 17.3555 5.4656C17.3789 5.55412 17.3825 5.64668 17.3661 5.73674L16.0601 12.9242C16.0339 13.0683 15.958 13.1985 15.8456 13.2923C15.7332 13.3861 15.5915 13.4375 15.4451 13.4375H5.18665C5.04034 13.4375 4.89866 13.3862 4.78631 13.2924C4.67395 13.1987 4.59804 13.0686 4.57179 12.9246L2.65034 2.38788C2.6241 2.24394 2.54818 2.11377 2.43583 2.02005C2.32347 1.92633 2.1818 1.875 2.03548 1.875H0.9375'
 let head_down_div4_p = document.createElement('p')
+head_down_div4_p.innerHTML = 'Корзина'
 let head_down_div5 = document.createElement('div')
 head_down_div5.classList.add('come_in si')
 let head_down_div5_svg = document.createElement('svg')
@@ -122,6 +136,7 @@ head_down_div5_svg.xmlns = 'http://www.w3.org/2000/svg'
 let head_down_div5_svg_path = document.createElement('path')
 head_down_div5_svg_path.d = "M15.71 12.71C16.6904 11.9387 17.406 10.8809 17.7572 9.68394C18.1085 8.48697 18.0779 7.21027 17.6698 6.03147C17.2617 4.85267 16.4963 3.83039 15.4801 3.10686C14.4639 2.38332 13.2474 1.99451 12 1.99451C10.7525 1.99451 9.53611 2.38332 8.51993 3.10686C7.50374 3.83039 6.73834 4.85267 6.33021 6.03147C5.92208 7.21027 5.89151 8.48697 6.24276 9.68394C6.59401 10.8809 7.3096 11.9387 8.29 12.71C6.61007 13.383 5.14428 14.4994 4.04889 15.9399C2.95349 17.3805 2.26956 19.0913 2.07 20.89C2.05555 21.0213 2.06711 21.1542 2.10402 21.2811C2.14093 21.4079 2.20246 21.5263 2.28511 21.6293C2.45202 21.8375 2.69478 21.9708 2.96 22C3.22521 22.0292 3.49116 21.9518 3.69932 21.7849C3.90749 21.618 4.04082 21.3752 4.07 21.11C4.28958 19.1552 5.22168 17.3498 6.68822 16.0388C8.15475 14.7278 10.0529 14.003 12.02 14.003C13.9871 14.003 15.8852 14.7278 17.3518 16.0388C18.8183 17.3498 19.7504 19.1552 19.97 21.11C19.9972 21.3557 20.1144 21.5827 20.2991 21.747C20.4838 21.9114 20.7228 22.0015 20.97 22H21.08C21.3421 21.9698 21.5817 21.8373 21.7466 21.6313C21.9114 21.4252 21.9881 21.1624 21.96 20.9C21.7595 19.0962 21.0719 17.381 19.9708 15.9382C18.8698 14.4954 17.3969 13.3795 15.71 12.71ZM12 12C11.2089 12 10.4355 11.7654 9.77772 11.3259C9.11992 10.8864 8.60723 10.2616 8.30448 9.53074C8.00173 8.79983 7.92251 7.99557 8.07686 7.21964C8.2312 6.44372 8.61216 5.73099 9.17157 5.17158C9.73098 4.61217 10.4437 4.2312 11.2196 4.07686C11.9956 3.92252 12.7998 4.00173 13.5307 4.30448C14.2616 4.60724 14.8863 5.11993 15.3259 5.77772C15.7654 6.43552 16 7.20888 16 8C16 9.06087 15.5786 10.0783 14.8284 10.8284C14.0783 11.5786 13.0609 12 12 12Z"
 let head_down_div5_p = document.createElement('p')
+head_down_div5_p.innerHTML = 'Войти'
 
 //2 create styling (css, attributes, text)
 
@@ -166,13 +181,18 @@ product_card_div.classList.add('img_box')
 let a_img = document.createElements('img')
 a_img.src = './imgs/noutbuk-lenovo-v15-igl-n4020-4256-gb-ssd-156-hd-185052-0.jpeg';
 let a_h2 = document.createElements('h2')
+a_h2.innerHTML = "Ноутбук Lenovo 81WQ001 XRK V15 N4020 4GB 1TB 15.6""
 let a_h2_br = document.createElements('br')
 let a_p1 = document.createElements('p')
+a_p1.innerHTML = "4 025 000"
 a_p1.classList.add('old_cost')
 let a_p2 = document.createElements('p')
+a_p2.innerHTML = "2 953 000"
 a_p2.classList.add('new_cost')
 let a_span1 = document.createElements('span')
+a_span1.innerHTML = 'Сум'
 let a_span2 = document.createElements('span')
+a_span1.innerHTML = 'Сум'
 let a_button = document.createElements('button')
 a_button.classList.add('muttaham_info')
 let a_muttaham_box = document.createElements('div')
@@ -181,6 +201,7 @@ let a_bag_box = document.createElements('button')
 a_bag_box.classList.add('bag_box');
 let a_muttaham_btn = document.createElements('button')
 a_muttaham_btn.classList.add('muttaham_btn')
+a_muttaham_btn.innerHTML = 'В рассрочку'
 let a_bag_box_svg = document.createElements('svg')
 a_bag_box_svg.width = 20
 a_bag_box_svg.height = 20
@@ -223,3 +244,4 @@ a_bag_box.append(a_bag_box_svg)
 a_bag_box_svg.append(a_bag_box_svg_path1, a_bag_box_svg_path2, a_bag_box_svg_path3)
 product_card_div.append(div_svg1, div_svg2)
 div_svg1.append(div_svg1_path)
+console.log();
